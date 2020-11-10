472f7fb4b0c08575c5a2660639dba75267ee87b1
Han Wang <hyw2@cornell.edu>
2020-11-10 17:59:01 -0500

    Add initial files

---
+++ js/app.js
@@ -0,0 +1,109 @@
+/* Template Name: sinata - Bootstrap 4 Landing Page Tamplate
+   Author: digitaltoch
+   File Description: Main JS file of the template
+*/
+
+
+! function($) {
+    "use strict";
+
+    var sinata = function() {};
+
+    sinata.prototype.initStickyMenu = function() {
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+        
+            if (scroll >= 50) {
+                $(".sticky").addClass("nav-sticky");
+            } else {
+                $(".sticky").removeClass("nav-sticky");
+            }
+        });
+    },
+
+    sinata.prototype.initSmoothLink = function() {
+        $('.navbar-nav a').on('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 0
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    },
+
+    sinata.prototype.initScrollspy = function() {
+        $("#navbarCollapse").scrollspy({
+            offset:20
+        });
+    },
+
+    sinata.prototype.initTesticarousel = function() {
+        $('#testi-carousel').owlCarousel({
+            items: 1,
+            loop:true,
+            margin:10,
+            nav:true,
+            responsive:{
+                576:{
+                    items:2
+                },
+     
+            }
+        });
+    },
+
+    sinata.prototype.initCounter = function() {
+        // Counter Number
+        var a = 0;
+        $(window).scroll(function() {
+            var oTop = $('#counter').offset().top - window.innerHeight;
+            if (a == 0 && $(window).scrollTop() > oTop) {
+                $('.counter-value').each(function() {
+                    var $this = $(this),
+                        countTo = $this.attr('data-count');
+                    $({
+                        countNum: $this.text()
+                    }).animate({
+                            countNum: countTo
+                        },
+
+                        {
+
+                            duration: 2000,
+                            easing: 'swing',
+                            step: function() {
+                                $this.text(Math.floor(this.countNum));
+                            },
+                            complete: function() {
+                                $this.text(this.countNum);
+                                //alert('finished');
+                            }
+
+                        });
+                });
+                a = 1;
+            }
+        });
+
+    },
+
+
+    feather.replace()
+
+    sinata.prototype.init = function() {
+        this.initStickyMenu();
+        this.initSmoothLink();
+        this.initScrollspy();
+        this.initTesticarousel();
+        this.initCounter();
+    },
+    //init
+    $.sinata = new sinata, $.sinata.Constructor = sinata
+}(window.jQuery),
+
+
+//initializing
+function($) {
+    "use strict";
+    $.sinata.init();
+}(window.jQuery);
---
+++ js/magnificpopup.init.js
@@ -0,0 +1,14 @@
+/* Template Name: sinata - Bootstrap 4 Landing Page Tamplate
+   Author: digitaltoch
+   File Description: Main JS file of the template
+*/
+
+//  VIDEO 
+$('.video-play-icon').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+});
